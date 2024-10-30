bd4b3339c007afe4b378c802c5cfc80fc0e7a655
maddie <60195170+madelineschwarz@users.noreply.github.com>
2024-10-30 07:43:38 -0700

    Update metashape_add_markers.py

--- metashape_add_markers.py
+++ metashape_add_markers.py
@@ -17,5 +17,10 @@ for point in chunk.markers:
     points_dic[point.label]=point.position
 
 # TODO: Read txt or csv file (long, lat, elevation)
+with open(r"elev_markers.csv", "r") as f:
+    reader = csv.reader(f, delimiter="\t")
+    for i, line in enumerate(reader):
+        print ('line[{}] = {}'.format(i, line))
+
 # TODO: Write markers from file (create markers)
 
